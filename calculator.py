40436a1f8197ee391f2838d139b0c2f99504a660
Kamzarr <75135894+Kamzarr@users.noreply.github.com>
2021-04-19 21:23:27 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
 import math
 
-cls, wnd = uic.loadUiType('calkulator.ui')
+cls, wnd = uic.loadUiType('calculator.ui')
 
 class Nasza (wnd, cls):
     def __init__(self):
